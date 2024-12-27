ec86436d00f890793188e513e47cd42baf8a06ed
Lucas Chaves <gabrielucas2008@hotmail.com>
2024-12-27 12:24:37 -0300

    first commit

---
+++ handler.py
@@ -0,0 +1,51 @@
+from typing import Any
+
+def handler(input: dict, context: object) -> dict[str, Any]:
+
+    timestamp = input.get("timestamp")
+    bytes_sent = input.get("net_io_counters_eth0-bytes_sent1")
+    bytes_recv = input.get("net_io_counters_eth0-bytes_recv1")
+    memory_cached = input.get("virtual_memory-cached")
+    memory_buffers = input.get("virtual_memory-buffers")
+    memory_total = input.get("virtual_memory-total")
+
+    env = context.env
+
+    percent_network_egress = (bytes_sent / (bytes_sent + bytes_recv)) * 100
+
+    percent_memory_caching  = ((memory_cached + memory_buffers) / memory_total) * 100
+
+    cpus = [key for key in input.keys() if 'cpu-percent-' in key]
+
+    cpus_avg = {key + '_avg': 0 for key in cpus}
+
+    for cpu_key in cpus:
+        cpu_usage = input.get(cpu_key)
+
+        cpu_states = env.get(cpu_key, [])
+        cpu_states.append(cpu_usage)
+
+        if(len(cpu_states) > 12):
+            cpu_states.pop(0)
+        
+        avg_usage = sum(cpu_states)/len(cpu_states)
+        cpus_avg[cpu_key + '_avg'] = avg_usage
+        env[cpu_key + '_avg'] = avg_usage
+        env[cpu_key] = cpu_states
+    
+    output = {
+        "timestamp": timestamp,
+        "percent-network-egress": percent_network_egress,
+        "percent-memory-caching": percent_memory_caching,
+        'cpus_avg': cpus_avg
+    }
+
+    return output
+        
+
+
+
+
+
+
+
