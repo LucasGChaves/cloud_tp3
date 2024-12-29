b69b95d4a07cf432a43519f1974006be39b63640
Lucas Chaves <gabrielucas2008@hotmail.com>
2024-12-29 01:28:14 -0300

    fixing runtime

--- runtime/runtime.py
+++ runtime/runtime.py
@@ -13,7 +13,7 @@ REDIS_INPUT_KEY = os.getenv("REDIS_INPUT_KEY", "metrics")
 REDIS_OUTPUT_KEY = os.getenv("REDIS_OUTPUT_KEY", "results")
 MONITOR_PERIOD = int(os.getenv("MONITOR_PERIOD", 5))
 ENTRY_FUNCTION = os.getenv("ENTRY_FUNCTION", "handler")
-ZIP_ENTRY_FILE = os.getenv("ZIP_ENTRY_FUNCTION", "handler")
+ZIP_ENTRY_FILE = os.getenv("ZIP_ENTRY_FUNCTION", "handler.py")
 ZIP_PATH = "/opt/usercode.zip"
 PYFILE_PATH = "/opt/usermodule.py"
 
@@ -64,9 +64,22 @@ def load_handler_from_zip(zip_path, zip_entry_file, entry_function):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_dir)
 
-    sys.path.append(extract_dir)
+        extracted_files = zip_ref.namelist()
+        entry_file_path = None
+        for file in extracted_files:
+            if file.endswith(zip_entry_file):
+                entry_file_path = os.path.join(extract_dir, file)
+                break
 
-    spec = importlib.util.spec_from_file_location(zip_entry_file, os.path.join(extract_dir, f"{zip_entry_file}.py"))
+        if not entry_file_path or not os.path.exists(entry_file_path):
+            raise FileNotFoundError(f"File '{zip_entry_file}' not found in the ZIP archive.")
+
+    sys.path.append(os.path.dirname(entry_file_path))
+
+    #sys.path.append(extract_dir)
+
+    spec = importlib.util.spec_from_file_location(zip_entry_file, entry_file_path)
+    #spec = importlib.util.spec_from_file_location(zip_entry_file, os.path.join(extract_dir, f"{zip_entry_file}"))
     user_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(user_module)
 
@@ -75,39 +88,35 @@ def load_handler_from_zip(zip_path, zip_entry_file, entry_function):
     return getattr(user_module, entry_function)
 
 def monitor_redis():
-    last_data = None
     handler = None
     context = Context()
 
     while True:
         try:
             data = redis_client.get(REDIS_INPUT_KEY)
+            print(data)
+            if data is not None:
+                input_data = json.loads(data)
 
-            if data != last_data:
-                last_data = data
+                zip_mtime = Path(ZIP_PATH).stat().st_mtime
 
-                if data is not None:
-                    input_data = json.loads(data.decode("utf-8"))
+                if handler is None:
+                    if os.path.isfile(ZIP_PATH):
+                        print("runtime: Using ZIP file.")
+                        handler = load_handler_from_zip(ZIP_PATH, ZIP_ENTRY_FILE, ENTRY_FUNCTION)
+                        context.function_getmtime = os.path.getmtime(ZIP_PATH)
+                    else:
+                        print("runtime: Using pyfile.")
+                        handler = load_handler_from_pyfile(PYFILE_PATH)
+                        context.function_getmtime = os.path.getmtime(PYFILE_PATH)
 
-                    zip_mtime = Path(ZIP_PATH).stat().st_mtime
+                    context.function_getmtime = zip_mtime
 
-                    if handler is None:
-                        if os.path.isfile(ZIP_PATH):
-                            print("runtime: Using ZIP file.")
-                            handler = load_handler_from_zip(ZIP_PATH, ZIP_ENTRY_FILE, ENTRY_FUNCTION)
-                            context.function_getmtime = os.path.getmtime(ZIP_PATH)
-                        else:
-                            print("runtime: Using pyfile.")
-                            handler = load_handler_from_pyfile(PYFILE_PATH)
-                            context.function_getmtime = os.path.getmtime(PYFILE_PATH)
+                context.last_execution = time.time()
 
-                        context.function_getmtime = zip_mtime
+                result = handler(input_data, context)
 
-                    context.last_execution = time.time()
-
-                    result = handler(input_data, context)
-
-                    redis_client.set(REDIS_OUTPUT_KEY, json.dumps(result).encode("utf-8"))
+                redis_client.set(REDIS_OUTPUT_KEY, json.dumps(result).encode("utf-8"))
 
         except Exception as e:
             print(f"Error: {e}")
